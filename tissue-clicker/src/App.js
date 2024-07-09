63fc664523e289662155bdd1ae4a3f15d23de195
MartyEternal <imartyl@hotmail.com>
2024-07-09 19:52:09 +1000

    first commit trying clicker game

---
+++ tissue-clicker/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [clickCount, setClickCount] = userState(0);
+
+  function handleClick() {
+    setClickCount(clickCount + 1);
+  }
+
+  function handleKeyPress() {
+    if (event.code === 'Space') {
+      setClickCount(clickCount + 1);
+    }
+  }
+
+  useEffect(function() {
+    window.addEventListener('keydown', handleKeyPress);
+
+    return function() {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [clickCount]);
+
+  return (
+    <div className="App" onClick={handleClick}>
+      <h1>Tissue Clicker Game</h1>
+      <p>Click Count: {clickCount}</p>
+      <div className="tissue-roll">
+        <p>🧻</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
