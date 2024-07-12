c0d49dac9c5719020eabc103a54518510517fb5b
MartyEternal <imartyl@hotmail.com>
2024-07-12 12:05:56 +1000

    settings menu nearly done. need break

--- tissue-clicker/src/App.js
+++ tissue-clicker/src/App.js
@@ -5,10 +5,14 @@ import Home from './Home';
 import Rollers from './Rollers';
 import Decor from './Decor';
 import About from './About';
+import Settings from './Settings';
 import { ClickProvider } from './ClickContext';
 
 function App() {
   const [activeMenu, setActiveMenu] = useState('');
+  const [darkMode, setDarkMode] = useState(false);
+  const [tissueGetBigger, setTissueGetBigger] = useState(true);
+  const [showTitle, setShowTitle] = useState(true);
 
   function toggleMenu(menu) {
     setActiveMenu(activeMenu === menu ? '' : menu);
@@ -26,16 +30,33 @@ function App() {
     toggleMenu('about');
   }
 
+  function handleToggleSettings() {
+    toggleMenu('settings');
+  }
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
+  }
+
+  function toggleTissueGetBigger() {
+    setTissueGetBigger(!tissueGetBigger);
+  }
+
+  function toggleShowTitle() {
+    setShowTitle(!showTitle);
+  }
+
   return (
     <ClickProvider>
       <Router>
-        <div className="App">
+        <div className={`App ${darkMode ? 'dark-mode' : ''}`}>
           <nav>
             <ul>
               {/* <li><Link to="/">Home</Link></li> */}
               <li><Link to="#" onClick={handleToggleRollers}>Rollers</Link></li>
               <li><Link to="#" onClick={handleToggleDecor}>Decor</Link></li>
               <li><Link to="#" onClick={handleToggleAbout}>About</Link></li>
+              <li><Link to="#" onClick={handleToggleSettings}>Settings</Link></li>
             </ul>
           </nav>
           <div className="content">
@@ -43,6 +64,16 @@ function App() {
             {activeMenu === 'rollers' && <Rollers />}
             {activeMenu === 'decor' && <Decor />}
             {activeMenu === 'about' && <About />}
+            {activeMenu === 'settings' && (
+              <Settings
+                darkMode={darkMode}
+                toggleDarkMode={toggleDarkMode}
+                tissueGetBigger={tissueGetBigger}
+                toggleTissueGetBigger={toggleTissueGetBigger}
+                showTitle={showTitle}
+                toggleShowTitle={toggleShowTitle}
+              />
+            )}
           </div>
         </div>
       </Router>
