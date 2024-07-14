b08ac217da8fe598ba496775964597432d6a049d
MartyEternal <imartyl@hotmail.com>
2024-07-14 12:39:00 +1000

    todos part 4 done

--- general-assembly/first-approach-to-react/src/App.js
+++ general-assembly/first-approach-to-react/src/App.js
@@ -7,6 +7,7 @@ import SkillList from './SkillList'
 import "./ToDoListItem.css";
 import "./SkillListItem.css";
 import "./NewSkillForm.css";
+import NewToDoForm from './NewToDoForm';
 
 // const todos = [
 //   'Have Fun',
@@ -14,15 +15,24 @@ import "./NewSkillForm.css";
 //   'Learn the MERN Stack'
 // ];
 
-// const skills = [
-//   { name: "HTML", level: 5 },
-//   { name: "CSS", level: 3 },
-//   { name: "JavaScript", level: 4 },
-//   { name: "Python", level: 2 },
-// ];
+const skills = [
+  { name: "HTML", level: 5 },
+  { name: "CSS", level: 3 },
+  { name: "JavaScript", level: 4 },
+  { name: "Python", level: 2 },
+];
 
 function App() {
   // const [showTodos, setShowTodos] = useState(true)
+  // const [todos, setTodos] = useState([
+  //   'Have Fun',
+  //   'Learn React',
+  //   'Learn the MERN Stack'
+  // ]);
+
+  // function addTodo(todo) {
+  //   setTodos([...todos, todo]);
+  // }
 
   const [skills, setSkills] = useState([
     { name: "HTML", level: 5 },
@@ -34,7 +44,10 @@ function App() {
   return (
     <div className="App">
       {/* <h1>React To-Do</h1>
-      {showTodos && <ToDoList todos={todos} />} */}
+      <button onClick={() => setShowTodos(!showTodos)}>{showTodos ? "HIDE" : "SHOW"}</button>
+      {showTodos && <ToDoList todos={todos} />}
+      <hr />
+      <NewToDoForm addTodo={addTodo} /> */}
 
 
       <h1 className="teal-text">React Dev Skills</h1>
---
+++ general-assembly/first-approach-to-react/src/NewToDoForm.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function NewToDoForm({ addTodo }) {
+    const [newTodo, setNewTodo] = useState("");
+
+    function handleAddTodo(evt) {
+        evt.preventDefault();
+        addTodo(newTodo);
+        setNewTodo("");
+    }
+
+    return (
+        <>
+            <h2>New To-Do</h2>
+            <form onSubmit={handleAddTodo}>
+                <input
+                    value={newTodo}
+                    onChange={(evt) => setNewTodo(evt.target.value)}
+                    placeholder="To-Do"
+                    required
+                    pattern=".{4,}" />
+                <button type="submit">ADD TO-DO</button>
+            </form>
+        </>
+    );
+}
+
+export default NewToDoForm;
