b5fe85e31b7b6e16452811881dc486878ddf1375
MartyEternal <imartyl@hotmail.com>
2024-07-15 20:45:04 +1000

    skill list lab part 4 done

--- general-assembly/first-approach-to-react/src/App.js
+++ general-assembly/first-approach-to-react/src/App.js
@@ -1,39 +1,11 @@
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ToDoList from './ToDoList'
 import NewSkillForm from './NewSkillForm'
 import SkillList from './SkillList'
-import "./ToDoListItem.css";
 import "./SkillListItem.css";
 import "./NewSkillForm.css";
-import NewToDoForm from './NewToDoForm';
-
-// const todos = [
-//   'Have Fun',
-//   'Learn React',
-//   'Learn the MERN Stack'
-// ];
-
-const skills = [
-  { name: "HTML", level: 5 },
-  { name: "CSS", level: 3 },
-  { name: "JavaScript", level: 4 },
-  { name: "Python", level: 2 },
-];
 
 function App() {
-  // const [showTodos, setShowTodos] = useState(true)
-  // const [todos, setTodos] = useState([
-  //   'Have Fun',
-  //   'Learn React',
-  //   'Learn the MERN Stack'
-  // ]);
-
-  // function addTodo(todo) {
-  //   setTodos([...todos, todo]);
-  // }
-
   const [skills, setSkills] = useState([
     { name: "HTML", level: 5 },
     { name: "CSS", level: 3 },
@@ -41,25 +13,15 @@ function App() {
     { name: "Python", level: 2 },
   ])
 
+  function addSkill(skill) {
+    setSkills([...skills, skill]);
+  }
+
   return (
     <div className="App">
-      {/* <h1>React To-Do</h1>
-      <button onClick={() => setShowTodos(!showTodos)}>{showTodos ? "HIDE" : "SHOW"}</button>
-      {showTodos && <ToDoList todos={todos} />}
-      <hr />
-      <NewToDoForm addTodo={addTodo} /> */}
-
-
       <h1 className="teal-text">React Dev Skills</h1>
       <span><SkillList skills={skills} /></span>
-      <span><NewSkillForm /></span>
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header> */}
+      <span><NewSkillForm onSubmit={addSkill} /></span>
     </div>
   );
 }
--- general-assembly/first-approach-to-react/src/NewSkillForm.jsx
+++ general-assembly/first-approach-to-react/src/NewSkillForm.jsx
@@ -1,19 +1,53 @@
-function NewSkillForm() {
+import { useState } from "react";
+
+function NewSkillForm({ onSubmit }) {
+    const [newSkill, setNewSkill] = useState({
+        name: "",
+        level: 3,
+    });
+
+    function handleChange(evt) {
+        const { name, value } = evt.target;
+        setNewSkill({
+            ...newSkill,
+            [name]: name === "level" ? parseInt(value) : value,
+        });
+    }
+
+    function handleAddSkill(evt) {
+        evt.preventDefault();
+        onSubmit(newSkill);
+        setNewSkill({ name: "", level: 3 });
+    }
+
     return (
         <div>
-            <form className="NewSkillForm" action="" method="POST">
-                <label for="skill">Skill:</label>
-                <input type="text" id="skill" name="skill" />
-                
-                <label for="level">Level:</label>
-                <select id="level" name="level">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
+            <form className="NewSkillForm" onSubmit={handleAddSkill}>
+                <label for="skill">Skill</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={newSkill.name}
+                    onChange={handleChange}
+                    required
+                    placeholder="New Skill Here"
+                />
+
+                <label for="level">Level</label>
+                <select
+                    id="level"
+                    name="level"
+                    value={newSkill.level}
+                    onChange={handleChange}
+                >
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
                 </select>
-                
+
                 <button type="submit">ADD SKILL</button>
             </form>
         </div>
