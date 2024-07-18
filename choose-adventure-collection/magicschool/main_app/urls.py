414e7aae49596604590722e33bc5aaa1627e4363
MartyEternal <imartyl@hotmail.com>
2024-07-18 11:32:58 +1000

    more tidying up

--- choose-adventure-collection/magicschool/main_app/urls.py
+++ choose-adventure-collection/magicschool/main_app/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('adventures/', views.adventures_index, name='index'),
     path('adventures/<int:adventure_id>/', views.adventures_detail, name='detail'),
     path('adventures/create/', views.AdventureCreate.as_view(), name='adventures_create'),
-    path('adventures/<int:pk>/update', views.AdventureUpdate.as_view(), name='adventures_update'),
-    path('adventures/<int:pk>/delete', views.AdventureDelete.as_view(), name='adventures_delete'),
+    path('adventures/<int:pk>/update/', views.AdventureUpdate.as_view(), name='adventures_update'),
+    path('adventures/<int:pk>/delete/', views.AdventureDelete.as_view(), name='adventures_delete'),
 
     # Magic School pathing
-    path('adventures/magicschool/000_intro.html', views.AdventureDelete.as_view(), name='adventures_delete'),
+    path('adventures/magicschool/000_intro/', views.intro_view, name='intro_view'),
 ]
